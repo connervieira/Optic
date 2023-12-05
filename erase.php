5ef41edbb2f09dbfb42d4a2a2b8aba73a4ee7e68
Conner Vieira <cvieira@v0lttech.com>
2023-12-05 13:11:30 -0500

    Added basic file management

---
+++ erase.php
@@ -0,0 +1,50 @@
+<?php
+include "./config.php";
+include "./utils.php";
+
+$force_login_redirect = true;
+include "./authentication.php";
+
+
+
+
+$instance_config = load_instance_config($config);
+$working_directory_files = $instance_config["general"]["working_directory"]; // Scan all files in the Predator working directory.
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title><?php echo $config["product_name"]; ?> - Storage</title>
+        <link rel="stylesheet" href="./styles/main.css">
+        <?php include "./loadtheme.php"; ?>
+        <link rel="stylesheet" href="./fonts/lato/latofonts.css">
+    </head>
+    <body>
+        <div class="navbar" role="navigation">
+            <a class="button" role="button" href="./storage.php">Back</a>
+        </div>
+        <h1><?php echo $config["product_name"]; ?></h1>
+        <h2>Erase</h2>
+        <br>
+        <main>
+            <?php
+            if (time() - $_GET["confirmation"] < 0) {
+                echo "<p>The confirmation timestamp is in the future. If you clicked an external link to get here, it is possible someone is attempt to manipulate you into erasing your dashcam video. No videos were deleted.</p>";
+            } else if (time() - $_GET["confirmation"] < 10) {
+                $erase_path = $instance_config["general"]["working_directory"] . "/" . "predator_dashcam*";
+                $erase_path = str_replace(" ", "\\ ", $erase_path);
+                $erase_path = str_replace("'", "\\'", $erase_path);
+                $erase_path = str_replace('"', '\\"', $erase_path);
+                shell_exec("sudo -u " . $config["exec_user"] . " rm " . $erase_path);
+                echo "<p>Successfully erased all unlocked dashcam video.</p>";
+            } else {
+                echo "<p>Are you sure you would like to permanently delete all unlocked dashcam videos?</p>";
+                echo "<a class='button' href='?confirmation=" . time() . "'>Confirm</a>";
+            }
+            ?>
+        </main>
+    </body>
+</html>
---
+++ storage.php
@@ -0,0 +1,74 @@
+<?php
+include "./config.php";
+
+$force_login_redirect = true;
+include "./authentication.php";
+
+include "./utils.php";
+
+
+
+$instance_config = load_instance_config($config);
+$working_directory_files = scandir($instance_config["general"]["working_directory"]); // Scan all files in the Predator working directory.
+
+$processed_videos = array(); // This array will hold each video and its processed information.
+$current_video = 0; // This is a placeholder that will hold the starting time of the first video segment from each continuous video.
+foreach ($working_directory_files as $file) { // Iterate through each file in the working directory.
+    if (strpos($file, "predator_dashcam") !== false) { // Check to see if this file is a dashcam video.
+        $processed_videos[$file]["size"] = filesize($instance_config["general"]["working_directory"] . "/" . $file);
+        $processed_videos[$file]["time"] = explode("_", $file)[2];
+        $processed_videos[$file]["device"] = explode("_", $file)[3];
+        if (intval(explode("_", $file)[4]) == 0) { // Check to see if this segment is at the start of a new video.
+            $current_video = $processed_videos[$file]["time"]; // Update the current video.
+        }
+        $processed_videos[$file]["video"] = $current_video;
+    }
+}
+
+$indexed_videos = array(); // This array will hold each continuous video and its individual segments.
+foreach ($processed_videos as $filename => $video) {
+    $video["file"] = $filename;
+    if (!isset($indexed_videos[$video["video"]])) {
+        $indexed_videos[$video["video"]] = array();
+    }
+    array_push($indexed_videos[$video["video"]], $video);
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title><?php echo $config["product_name"]; ?> - Storage</title>
+        <link rel="stylesheet" href="./styles/main.css">
+        <?php include "./loadtheme.php"; ?>
+        <link rel="stylesheet" href="./fonts/lato/latofonts.css">
+    </head>
+    <body>
+        <div class="navbar" role="navigation">
+            <a class="button" role="button" href="./index.php">Back</a>
+        </div>
+        <h1><?php echo $config["product_name"]; ?></h1>
+        <h2>Storage</h2>
+        <br><a class="button" role="button" style="background:#ff4444;color:black;" href="./erase.php">Erase</a><br>
+        <br>
+        <main>
+            <div class="list">
+                <?php
+                if (sizeof($indexed_videos) > 0) {
+                    foreach ($indexed_videos as $timestamp => $video) {
+                        echo "<hr>";
+                        echo "<h4>" . date("Y-m-d H:i:s", $timestamp) . "</h4>";
+                        echo "<ul>";
+                        foreach ($video as $number => $segment) {
+                            echo "<li><a href='./download.php?video=" . $segment["file"] . "'>" . date("H:i:s", $segment["time"]) . "</a></li>";
+                        }
+                        echo "</ul>";
+                    }
+                } else {
+                    echo "<p><i>No dashcam video has been recorded.</i></p>";
+                }
+                ?>
+            </div>
+        </main>
+    </body>
+</html>
