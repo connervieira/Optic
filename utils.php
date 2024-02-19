07f0cfd81dd58d5826b499ca7ccee24cf947098d
Conner Vieira <cvieira@v0lttech.com>
2024-02-18 20:32:30 -0500

    Added interface directory check

--- utils.php
+++ utils.php
@@ -124,8 +124,11 @@ function verify_permissions($config) {
         $valid = false;
     }
 
+
     $instance_config = load_instance_config($config);
-    if (is_dir($instance_config["general"]["interface_directory"]) == false) { // Check to make sure the specified interface directory exists.
+    if ($instance_config["interface_directory"] == "") {
+        echo "<p class=\"warning\">The interface directory is disabled in Predator's configuration. Predator needs the interface directory to be enabled in order for the control interface to communicate with it.</p>";
+    } else if (is_dir($instance_config["general"]["interface_directory"]) == false) { // Check to make sure the specified interface directory exists.
         //echo "<p class=\"warning\">The interface directory doesn't exist. Please verify that the correct interface directory is configured in the settings.</p>";
     } else if (is_writable($instance_config["general"]["interface_directory"]) == false) { // Check to see if the interface directory is writable.
         echo "<p class=\"warning\">The interface directory isn't writable. Please verify that the interface directory at " . $instance_config["general"]["interface_directory"] . " has the correct permissions.</p>";
