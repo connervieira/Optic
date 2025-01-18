779b130baf818b0ff5bbe14ba204e926b0495e6c
Conner Vieira <cvieira@v0lttech.com>
2025-01-17 20:57:46 -0500

    Added support for Predator V12

--- storagenormal.php
+++ storagenormal.php
@@ -7,69 +7,7 @@ include "./authentication.php";
 
 
 $instance_config = load_instance_config($config);
-if (isset($instance_config["dashcam"]["saving"]["segment_length"])) { // This is for compatibility with Predator V10.
-    $segment_length = $instance_config["dashcam"]["saving"]["segment_length"];
-} else if (isset($instance_config["dashcam"]["capture"]["opencv"]["segment_length"])) { // This is for compatibility with pre-release versions of Predator V10.
-    $segment_length = $instance_config["dashcam"]["capture"]["opencv"]["segment_length"];
-}
-
-$directory_files = scandir($instance_config["general"]["working_directory"]); // Scan all files in the Predator working directory.
-$filtered_directory_files = array(); // This is a placeholder that will hold a list of all relevant files from the Predator working directory.
-foreach ($directory_files as $file) {
-    if (strpos($file, "predator_dashcam") !== false and (substr($file, -3) == "mkv" or substr($file, -3) == "avi" or substr($file, -3) == "m4v" or substr($file, -3) == "mp4")) { // Check to see if this file is a dashcam video.
-        if ($_GET["show"] == "normal") { // Check to see if only normal videos should be displayed.
-            if (strtoupper(explode("_", $file)[5][0]) == "N") { // Check to see if this segment is a normal video.
-                array_push($filtered_directory_files, $file); // Add this file to the list of filtered files.
-            }
-        } else if ($_GET["show"] == "parked") { // Check to see if this segment is a parked video.
-            if (strtoupper(explode("_", $file)[5][0]) == "P") { // Check to see if this segment is a parked video.
-                array_push($filtered_directory_files, $file); // Add this file to the list of filtered files.
-            }
-        } else { // Otherwise, show all video segments.
-            array_push($filtered_directory_files, $file); // Add this file to the list of filtered files.
-        }
-    }
-}
-
-$processed_videos = array(); // This array will hold each video and its processed information.
-$current_video = 0; // This is a placeholder that will hold the starting time of the first video segment from each continuous video.
-foreach ($filtered_directory_files as $file) { // Iterate through each file in the working directory.
-    $processed_videos[$file]["size"] = filesize($instance_config["general"]["working_directory"] . "/" . $file);
-    $processed_videos[$file]["time"] = explode("_", $file)[2];
-    $processed_videos[$file]["device"] = explode("_", $file)[3];
-    $processed_videos[$file]["video"] = $processed_videos[$file]["time"]; // This determines the full continuous video that this segment is a part of.
-    $processed_videos[$file]["segment"] = intval(explode("_", $file)[4]);
-    $processed_videos[$file]["mode"] = strtoupper(explode("_", $file)[5][0]);
-
-    # Check to see if there is an audio file associated with this video file.
-    $base_filename = $instance_config["general"]["working_directory"] . "/" . substr($file, 0, -3);
-    if (file_exists($base_filename . "wav")) { $processed_videos[$file]["audio"] = substr($file, 0, -3) . "wav";
-    } else if (file_exists($base_filename . "mp3")) { $processed_videos[$file]["audio"] = substr($file, 0, -3) . "mp3";
-    } else if (file_exists($base_filename . "flac")) { $processed_videos[$file]["audio"] = substr($file, 0, -3) . "flac";
-    } else if (file_exists($base_filename . "ogg")) { $processed_videos[$file]["audio"] = substr($file, 0, -3) . "ogg";
-    }
-
-    $time_since_previous = $processed_videos[$file]["time"] - $last_video_time; // Calculate the time difference between this segment's timestamp and the last segment's timestamp.
-    if (($time_since_previous > $segment_length + 1 or $time_since_previous < $segment_length - 1) and $time_since_previous !== 0) { // Check to see if this segment is immediately after the previous segment, plus a margin of error.
-        $current_video = $processed_videos[$file]["time"]; // Make this segment the start of a new video set.
-    }
-    $processed_videos[$file]["video"] = $current_video; // Set this segment to be part of the current video set.
-
-    $last_video_time = $processed_videos[$file]["time"];
-}
-
-
-$indexed_videos = array(); // This array will hold each continuous video and its individual segments.
-foreach ($processed_videos as $filename => $video) {
-    $video["file"] = $filename;
-    if (!isset($indexed_videos[$video["video"]])) {
-        $indexed_videos[$video["video"]] = array();
-    }
-    if (!isset($indexed_videos[$video["video"]][$video["time"]])) {
-        $indexed_videos[$video["video"]][$video["time"]] = array();
-    }
-    $indexed_videos[$video["video"]][$video["time"]][$video["device"]] = $video;
-}
+$indexed_videos = index_videos($instance_config["general"]["working_directory"], $instance_config);
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -113,16 +51,15 @@ foreach ($processed_videos as $filename => $video) {
                             echo "<ul>";
                             foreach ($video as $time => $segment) {
                                 echo "<li>" . date("H:i:s", $time + (3600*$config["timestamp_offset"])) . " -";
+                                $audio = ""; // This is a placeholder that will be replaced with this segment's audio file (if one exists).
                                 foreach ($segment as $device) {
                                     if (isset($device["audio"])) {
-                                        echo " <span>" . $device["device"] . " (</span>";
-                                        echo "<a href='./downloadnormal.php?video=" . $device["file"] . "'>video</a>";
-                                        echo "<span>/</span>";
-                                        echo "<a href='./downloadnormal.php?video=" . $device["audio"] . "'>audio</a>";
-                                        echo "<span>)</span>";
-                                    } else {
-                                        echo " <a href='./downloadnormal.php?video=" . $device["file"] . "'>" . $device["device"] .  "</a>";
+                                        $audio = $device["audio"];
                                     }
+                                    echo " <a href='./downloadnormal.php?video=" . $device["file"] . "'>" . $device["device"] . "</a>";
+                                }
+                                if (strlen($audio) > 0) { // Check to see if there is an associated audio file.
+                                    echo " <a href='./downloadnormal.php?video=" . $audio . "'>(audio)</a>";
                                 }
                                 echo "</li>";
                             }
--- storagesaved.php
+++ storagesaved.php
@@ -5,47 +5,8 @@ include "./utils.php";
 $force_login_redirect = true;
 include "./authentication.php";
 
-
 $instance_config = load_instance_config($config);
-if (isset($instance_config["dashcam"]["saving"]["segment_length"])) { // This is for compatibility with Predator V10.
-    $segment_length = $instance_config["dashcam"]["saving"]["segment_length"];
-} else if (isset($instance_config["dashcam"]["capture"]["opencv"]["segment_length"])) { // This is for compatibility with pre-release versions of Predator V10.
-    $segment_length = $instance_config["dashcam"]["capture"]["opencv"]["segment_length"];
-}
-$directory_files = scandir($instance_config["general"]["working_directory"] . "/" . $instance_config["dashcam"]["saving"]["directory"]); // Scan all files in the saved dashcam video directory.
-
-$processed_videos = array(); // This array will hold each video and its processed information.
-$current_video = 0; // This is a placeholder that will hold the starting time of the first video segment from each continuous video.
-foreach ($directory_files as $file) { // Iterate through each file in the working directory.
-    if (strpos($file, "predator_dashcam") !== false) { // Check to see if this file is a dashcam video.
-        $processed_videos[$file]["size"] = filesize($instance_config["general"]["working_directory"] . "/" . $file);
-        $processed_videos[$file]["time"] = explode("_", $file)[2];
-        $processed_videos[$file]["device"] = explode("_", $file)[3];
-        $processed_videos[$file]["video"] = $processed_videos[$file]["time"];
-        $processed_videos[$file]["segment"] = intval(explode("_", $file)[4]);
-        $processed_videos[$file]["mode"] = explode("_", $file)[5];
-
-        $time_since_previous = $processed_videos[$file]["time"] - $last_video_time; // Calculate the time difference between this segment's timestamp and the last segment's timestamp.
-        if (($time_since_previous > $segment_length + 1 or $time_since_previous < $segment_length - 1) and $time_since_previous !== 0) { // Check to see if this segment is immediately after the previous segment, plus a margin of error.
-            $current_video = $processed_videos[$file]["time"]; // Make this segment the start of a new video set.
-        }
-        $processed_videos[$file]["video"] = $current_video; // Set this segment to be part of the current video set.
-
-        $last_video_time = $processed_videos[$file]["time"];
-    }
-}
-
-$indexed_videos = array(); // This array will hold each continuous video and its individual segments.
-foreach ($processed_videos as $filename => $video) {
-    $video["file"] = $filename;
-    if (!isset($indexed_videos[$video["video"]])) {
-        $indexed_videos[$video["video"]] = array();
-    }
-    if (!isset($indexed_videos[$video["video"]][$video["time"]])) {
-        $indexed_videos[$video["video"]][$video["time"]] = array();
-    }
-    $indexed_videos[$video["video"]][$video["time"]][$video["device"]] = $video;
-}
+$indexed_videos = index_videos($instance_config["general"]["working_directory"] . "/" . $instance_config["dashcam"]["saving"]["directory"], $instance_config);
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -77,15 +38,22 @@ foreach ($processed_videos as $filename => $video) {
                             echo "<ul>";
                             foreach ($video as $time => $segment) {
                                 echo "<li>" . date("H:i:s", $time + (3600*$config["timestamp_offset"])) . " -";
+                                $audio = ""; // This is a placeholder that will be replaced with this segment's audio file (if one exists).
                                 foreach ($segment as $device) {
-                                    echo " <a href='./downloadsaved.php?video=" . $device["file"] . "'>" . $device["device"] .  "</a>";
+                                    if (isset($device["audio"])) {
+                                        $audio = $device["audio"];
+                                    }
+                                    echo " <a href='./downloadnormal.php?video=" . $device["file"] . "'>" . $device["device"] . "</a>";
+                                }
+                                if (strlen($audio) > 0) { // Check to see if there is an associated audio file.
+                                    echo " <a href='./downloadnormal.php?video=" . $audio . "'>(audio)</a>";
                                 }
                                 echo "</li>";
                             }
                             echo "</ul>";
                         }
                     } else {
-                        echo "<p><i>No dashcam video has been recorded.</i></p>";
+                        echo "<p><i>No dashcam video has been saved.</i></p>";
                     }
                 }
                 ?>
